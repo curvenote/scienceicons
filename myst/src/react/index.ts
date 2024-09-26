82e91ccc092f04bbc128e38f5f85c0eb06489b94
Rowan Cockett <rowanc1@gmail.com>
2024-09-26 14:27:52 -0600

    📦 v1.03 for myst

--- myst/src/react/index.ts
+++ myst/src/react/index.ts
@@ -1,1 +1,9 @@
-export { ScienceIconRenderer } from './scienceicons.js'
+import { type NodeRenderers } from '@myst-theme/providers';
+import { ScienceIconRenderer } from './scienceicons.js';
+export { ScienceIconRenderer } from './scienceicons.js';
+
+const SCIENCEICON_RENDERERS: NodeRenderers = {
+  scienceicon: ScienceIconRenderer,
+};
+
+export default SCIENCEICON_RENDERERS;
