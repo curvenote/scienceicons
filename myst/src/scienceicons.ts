ea74ab1dffa70089e50a4cd2af308533ba5ad372
Rowan Cockett <rowanc1@gmail.com>
2024-09-26 14:23:15 -0600

    📦 Update packaging for myst

--- myst/src/scienceicons.ts
+++ myst/src/scienceicons.ts
@@ -1,9 +1,11 @@
-import { fileWarn } from 'myst-common'
-import names from './names.json'
+import type { CurvenotePlugin } from '@curvenote/common';
+import { fileWarn } from 'myst-common';
+import type { GenericNode, RoleSpec } from 'myst-common';
+import names from './names.json';
 
-const SUPPORTED_ICONS = names.map((icon) => icon.name)
+const SUPPORTED_ICONS = names.map((icon) => icon.name);
 
-const iconRole = {
+const iconRole: RoleSpec = {
   name: 'scienceicon',
   alias: ['scicon'],
   body: {
@@ -11,15 +13,16 @@ const iconRole = {
     required: true,
     doc: 'The kind of icon to display',
   },
-  run(data, vfile, ctx) {
-    if (!SUPPORTED_ICONS.includes(data.body)) {
-      return fileWarn(vfile, `Unknown kind of scienceicon: ${data.body}`)
+  run(data, vfile) {
+    if (!SUPPORTED_ICONS.includes(data.body as string)) {
+      fileWarn(vfile, `Unknown name of scienceicon: ${data.body}`);
+      return [];
     }
 
-    return [{ type: 'scienceicon', value: data.body, kind: data.body }]
+    return [{ type: 'scienceicon', value: data.body, kind: data.body }] as GenericNode[];
   },
-}
+};
 
-const plugin = { name: 'Science Icons by Curvenote', roles: [iconRole] }
+const plugin = { name: 'Science Icons by Curvenote', roles: [iconRole] } as CurvenotePlugin;
 
-export default plugin
+export default plugin;
